eaf9664abb1ee4c544bd11390a1a5b2103d2f1fb
areeba <areeba@perfectprice.com>
2021-02-05 19:25:46 -0500

    Add baseline model.

--- app/src/main/java/com/example/tflitemodelbenchmarking/MainActivity.java
+++ app/src/main/java/com/example/tflitemodelbenchmarking/MainActivity.java
@@ -24,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
   private static final String XRAY_DIR = "xrayImages";
   private static final int toastDuration = Toast.LENGTH_SHORT;
   private static PowerManager.WakeLock wakeLock;
-
+  private static String[] labels = {"Atelectasis", "Cardiomegaly", "Effusion", "Infiltration",
+    "Mass", "Nodule", "Pneumonia", "Pneumothorax", "Consolidation", "Edema", "Emphysema",
+    "Fibrosis", "Pleural_Thickening", "Hernia"};
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +41,26 @@ public class MainActivity extends AppCompatActivity {
   }
 
   public void run_all(View view) throws IOException {
-    runTest("chexnet_pruned_model.tflite");
-    runTest("chexnet_quant.tflite");
-    runTest("chexnet_quant2.tflite");
-    runTest("chexnet_pruned_quant.tflite");
+    for (int i = 0; i < 10; i++) {
+
+      runTest("chexBaselineModel.tflite"); // from bruce chou's repository
+
+      runTest("chexnet_pruned_model.tflite");
+      runTest("chexnet_pruned_quant.tflite");
+      runTest("chexnet_quant.tflite");
+      runTest("chexnet_quant2.tflite");
+
+//       Below models throw error
+//      runTest("chexnet_pruned_quant_f16.tflite");
+//      runTest("chexnet_pruned_quant_io.tflite");
+//      runTest("chexnet_quant_f16.tflite");
+//      runTest("chexnet_rsna_quant_io.tflite");
+//      runTest("chexnet_ts_quant_io.tflite");
+    }
+  }
+
+  public void run_baseline(View view) throws IOException {
+    runTest("chexBaselineModel.tflite");
   }
 
   public void run_chexnet_pruned_quant(View view) throws IOException {
@@ -64,51 +82,32 @@ public class MainActivity extends AppCompatActivity {
   public void runTest(String modelName) throws IOException {
     wakeLock.acquire(10*60*1000L /*10 minutes*/);
 
-    Context context = getBaseContext();
     float startTime = System.nanoTime();
     float testTime = 0.0f;
     float minLatency = Float.MAX_VALUE;
     float maxLatency = Float.MIN_VALUE;
 
-    Toast toast = Toast.makeText(context,
-                                    "Test started; avg latency: ",
-                                    Toast.LENGTH_LONG);
-
     String[] xrayList = getAssets().list(XRAY_DIR);
     int numImages = xrayList.length;
     if (numImages > 0) {
       // Run test on all images in directory
-//      Toast toast = Toast.makeText(context, "Running test. This can take a few minutes.", Toast.LENGTH_LONG);
-      toast.show();
-      int counter = 1;
-
       for (String xray : xrayList) {
-//        Log.v(TAG, "Running inference on image: " + xray);
-//        // Pass image into model, time the inference, and close the model
-//        float runTime = timeInference(modelName, xray);
-//        testTime += runTime;
-//        minLatency = Math.min(minLatency, runTime);
-//        maxLatency = Math.max(maxLatency, runTime);
-
-        // Update user
-        toast = Toast.makeText(context, "Running test. Completed " + counter++ + " / " + numImages, toastDuration);
-        toast.show();
+        Log.v(TAG, "Running inference on image: " + xray);
+        // Pass image into model, time the inference, and close the model
+        float runTime = timeInference(modelName, xray);
+        testTime += runTime;
+        minLatency = Math.min(minLatency, runTime);
+        maxLatency = Math.max(maxLatency, runTime);
       }
 
       // Log stats
       float endTime = System.nanoTime();
-      Log.d(TAG, "Completed test of " + modelName + " in " + (endTime - startTime) + " ns.");
+      Log.d(TAG, "Completed test of " + modelName + " in " + (endTime - startTime) + " ns on " + numImages + " images.");
       Log.d(TAG, "Average inference time per image: " + (testTime / numImages));
       Log.d(TAG, "Min latency: " + minLatency + " ns.");
       Log.d(TAG, "Max latency: " + maxLatency + " ns.");
-//      toast = Toast.makeText(context,
-//                                    "Test completed; avg latency: " + (testTime / numImages),
-//                                    Toast.LENGTH_LONG);
-      toast.show();
     } else {
       Log.e(TAG, "No files found. Tests not run.");
-//      Toast toast = Toast.makeText(context, "Error: No tests were run. See logs", Toast.LENGTH_LONG);
-//      toast.show();
     }
     wakeLock.release();
   }
@@ -116,11 +115,11 @@ public class MainActivity extends AppCompatActivity {
   private float timeInference(String modelName, String image) throws IOException {
     float[][][][] input = Utils.loadImageAsFloatArr(getApplicationContext(), image);
     float[][] output = new float[1][14];
-    float time = System.nanoTime();
 
     Interpreter interpreter =
       new Interpreter(Utils.loadTfliteModel(getApplicationContext(), modelName));
     Log.v(TAG, "Model loaded: " + modelName);
+    float time = System.nanoTime();
     interpreter.run(input, output);
     float runTime = System.nanoTime() - time;
     interpreter.close();
@@ -132,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
 //    toast.show();
     Log.v(TAG, "Output: " + Arrays.toString(output[0]));
     Log.v(TAG, "Likeliest index: " + Utils.getIndexOfLargest(output[0]));
+    Log.v(TAG, "Likeliest label: " + labels[Utils.getIndexOfLargest(output[0])]);
     Log.v(TAG, "Inference done in " + runTime + " ns.");
 
     return runTime;
