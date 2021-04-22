168a8891f07647ff8a1f519e9c5b3ae13b5e9fd6
areeba <areeba@perfectprice.com>
2021-04-22 13:26:13 -0400

    Update quant-aware trained model. Print results to screen.

--- app/src/main/java/com/example/tflitemodelbenchmarking/MainActivity.java
+++ app/src/main/java/com/example/tflitemodelbenchmarking/MainActivity.java
@@ -13,9 +13,9 @@ import android.view.MenuItem;
 import android.util.Log;
 import android.view.WindowManager;
 import android.widget.Toast;
+import android.widget.TextView;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 import org.tensorflow.lite.Interpreter;
 //import org.tensorflow.lite.gpu.CompatibilityList;
@@ -47,49 +47,58 @@ public class MainActivity extends AppCompatActivity {
   }
 
   public void run_all(View view) throws IOException {
-    for (int i = 0; i < 10; i++) {
-      runTest("chexnet_kaggle_tflite_int8_quantization.tflite"); // throws error: Caused by: java.lang.IllegalArgumentException: Cannot convert between a TensorFlowLite tensor with type UINT8 and a Java object of type [[[[F (which is compatible with the TensorFlowLite type FLOAT32).
-      runTest("chexnet_kaggle_tflite_no_quantization.tflite");
-      runTest("chexnet_kaggle_tflite_dynamic_quantization.tflite");
-//      runTest("chexnet_float_ip_op_int8_quantized.tflite");
-//      runTest("chexnet_kaggle_tflite_float_ip_op_int8_quantization.tflite");
-    }
+    int numIterations = 1;
+
+    runTestNTimes("chexnet_kaggle_tflite_no_quantization.tflite", numIterations, MODEL_TYPE.FLOAT_MODEL, "Baseline");
+    runTestNTimes("QAT_int_8_v1.tflite", numIterations, MODEL_TYPE.FLOAT_MODEL, "Quant-Aware Int8");
+    runTestNTimes("chexnet_kaggle_tflite_dynamic_quantization.tflite", numIterations, MODEL_TYPE.FLOAT_MODEL, "Dynamic Quant");
+    Log.d(TAG, "Done with all tests.");
   }
 
-  public void runTest(String modelName) throws IOException {
+  public float runTestNTimes(String modelName, int numIterations, MODEL_TYPE modelType, String label) throws IOException {
     wakeLock.acquire(10*60*1000L /*10 minutes*/);
+    Log.d(TAG, "Running test for " + modelName + " " + numIterations + " times.");
 
     float testStartTimeMs = SystemClock.uptimeMillis();
     float testTime = 0.0f;
+    float avgTimePerImage;
     float minLatency = Float.MAX_VALUE;
     float maxLatency = Float.MIN_VALUE;
 
     String[] xRayList = getAssets().list(XRAY_DIR);
     int numImages = xRayList.length;
+
     if (xRayList.length == 0) {
       Log.e(TAG, "No xray images found. Tests not run.");
+      return Float.NaN;
     } else {
-      // Run test on all images in directory
-      for (String xray : xRayList) {
-        Log.v(TAG, "Running inference on image: " + xray);
-        // Pass image into model, time the inference, and close the model
-        float runTime = timeInference(modelName, xray);
-        testTime += runTime;
-        minLatency = Math.min(minLatency, runTime);
-        maxLatency = Math.max(maxLatency, runTime);
+      for (int i = 0; i < numIterations; i++) {
+        // Run test on all images in directory
+        for (String xray : xRayList) {
+          Log.v(TAG, "Running inference on image: " + xray);
+          // Pass image into model, time the inference, and close the model
+          float runTime = timeInference(modelName, xray, modelType);
+          testTime += runTime;
+          minLatency = Math.min(minLatency, runTime);
+          maxLatency = Math.max(maxLatency, runTime);
+        }
       }
 
       // Log stats
       float endTimeMs = SystemClock.uptimeMillis();
-      Log.d(TAG, "Completed test of " + modelName + " in " + (endTimeMs - testStartTimeMs) + " ms on " + numImages + " images.");
-      Log.d(TAG, "Average inference time per image: " + (testTime / numImages) + "for model: " + modelName);
+      avgTimePerImage = testTime / numImages;
+      Log.d(TAG, "Completed test of " + label + " Model in " + (endTimeMs - testStartTimeMs) + " ms on " + numImages + " images.");
+      Log.d(TAG, "Average inference time per image: " + avgTimePerImage + " for model: " + modelName);
       Log.d(TAG, "Min latency: " + minLatency + " ms.");
       Log.d(TAG, "Max latency: " + maxLatency + " ms.");
     }
     wakeLock.release();
+    TextView resultsBox = (TextView) findViewById(R.id.resultsView);
+    resultsBox.append("\n" + label + ": " + avgTimePerImage + " ms" + "\n");
+    return avgTimePerImage;
   }
 
-  private float timeInference(String modelName, String image) throws IOException {
+  private float timeInference(String modelName, String image, MODEL_TYPE modelType) throws IOException {
 //    // Set interpreter options for CPU/GPU
 //    if(compatList.isDelegateSupportedOnThisDevice()){
 //      // if the device has a supported GPU, add the GPU delegate
@@ -112,24 +121,22 @@ public class MainActivity extends AppCompatActivity {
       Log.e(TAG, "Creating interpreter FAILED with msg: " + e.getMessage());
       return -1.0f;
     }
-    Log.v(TAG, "Model loaded: " + modelName);
+//    Log.v(TAG, "Model loaded: " + modelName);
 
     // Run model, and time inference
     float time = SystemClock.uptimeMillis();
-    if (modelName.contains("int")) {
-      Log.d(TAG, "Preparing to run int model: " + modelName);
+    if (modelType == MODEL_TYPE.INT_MODEL) {
       byte[][][][] inputByte = Utils.loadImageAsByteArr(getApplicationContext(), image);
       byte[][] outputByte = new byte[1][14];
       interpreter.run(inputByte, outputByte);
-      Log.v(TAG, "Output: " + Arrays.toString(outputByte[0]));
-      Log.v(TAG, "Likeliest label: " + labels[Utils.getIndexOfLargestByte(outputByte[0])]);
+//      Log.v(TAG, "Output: " + Arrays.toString(outputByte[0]));
+//      Log.v(TAG, "Likeliest label: " + labels[Utils.getIndexOfLargestByte(outputByte[0])]);
     } else {
-      Log.d(TAG, "Preparing to run float model: " + modelName);
       float[][][][] inputFloat = Utils.loadImageAsFloatArr(getApplicationContext(), image);
       float[][] outputFloat = new float[1][14];
       interpreter.run(inputFloat, outputFloat);
-      Log.v(TAG, "Output: " + Arrays.toString(outputFloat[0]));
-      Log.v(TAG, "Likeliest label: " + labels[Utils.getIndexOfLargestFloat(outputFloat[0])]);
+//      Log.v(TAG, "Output: " + Arrays.toString(outputFloat[0]));
+//      Log.v(TAG, "Likeliest label: " + labels[Utils.getIndexOfLargestFloat(outputFloat[0])]);
     }
 
     // Log inference result and clean up
@@ -157,3 +164,6 @@ public class MainActivity extends AppCompatActivity {
     return super.onOptionsItemSelected(item);
   }
 }
+
+enum MODEL_TYPE {FLOAT_MODEL, INT_MODEL};
+
